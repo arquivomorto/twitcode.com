27150015f02c04ad74cf0ab25ca723f91e93a6ab
Hacker Gaucho <84669018+hackergaucho@users.noreply.github.com>
2022-06-18 18:19:04 -0300

    app/mix/Utils.php <- showErrors

---
+++ app/mix/Utils.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\mix;
+
+class Utils{
+	function showErrors($bool){
+		if($bool){
+			ini_set('display_errors',1);
+			ini_set('display_startup_errors',1);
+			error_reporting(E_ALL);
+		}else{
+			ini_set('display_errors',0);
+			ini_set('display_startup_errors',0);
+			error_reporting(0);
+		}
+	}
+}
--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
-	ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
-	error_reporting(E_ALL);
 require 'vendor/autoload.php';
+$cfg=require 'cfg.php';
 
 use app\controller\Home;
+use app\mix\Utils;
+
+$Utils=new Utils();
+$Utils->showErrors($cfg['showErrors']);
 
 $Home=new Home();
 $Home->index();
