79ef4b7badd063b09eb3ff538a99db77f05d3fdf
Hacker Gaucho <84669018+hackergaucho@users.noreply.github.com>
2022-06-18 17:43:58 -0300

    hello world

---
+++ app/controller/Home.php
@@ -0,0 +1,8 @@
+<?php
+namespace app\controller;
+
+class Home{
+	function index(){
+		print 'hello world';
+	}
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e5dd80ebcc2733956fc6bcf42a046e9a3fd7aa37',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e5dd80ebcc2733956fc6bcf42a046e9a3fd7aa37',
+            'dev_requirement' => false,
+        ),
+    ),
+);
