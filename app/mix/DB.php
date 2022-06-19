c6c70f4b02d950a8a776adc1c24d72600b224f6c
Hacker Gaucho <84669018+hackergaucho@users.noreply.github.com>
2022-06-18 22:13:22 -0300

    app/mix/DB.php

---
+++ app/mix/DB.php
@@ -0,0 +1,33 @@
+<?php
+namespace app\mix;
+
+use app\mix\Utils;
+
+use Pdo;
+use PDOException;
+
+class DB extends Utils{
+	var $pdoDsn,$pdoUsername,$pdoPassword;
+	function conn($name){
+		$cfg=parent::cfg()['db'][$name];
+		switch ($name) {
+			case 'mysql':
+
+				$pdoDb = $cfg['db'];
+			        $pdoHost = $cfg['host'];
+			        $this->pdoPassword = $cfg['password'];
+			        $this->pdoUsername = $cfg['user'];
+			        $this->pdoDsn = "mysql:host=$pdoHost;dbname=$pdoDb";
+		        	break;
+		    	case 'sqlite':
+			        $pdoDsn = "sqlite:" . $cfg['file'];
+			        break;
+		}
+		try {
+		    $conn = new PDO($this->pdoDsn, $this->pdoUsername, $this->pdoPassword);
+		} catch (PDOException $e) {
+		    print "Erro ao conectar: " . PHP_EOL . $e->getMessage();
+		}
+		return $conn;
+	}
+}
--- cfg.example.php
+++ cfg.example.php
@@ -1,4 +1,16 @@
 <?php
+$db=[
+	'mysql'=>[
+		'db'=>'twitcode',
+		'host'=>'localhost',
+		'user'=>'root',
+		'password'=>'senha1234'
+	],
+	'sqlite'=>[
+		'file'=>__DIR__.'/dir/db.sqlite3'
+	]
+];
 return [
-	'showErrors'=>true
+	'showErrors'=>true,
+	'db'=>$db
 ];
