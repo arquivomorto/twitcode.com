eb26579120c15e7baec55f1c37dc4b6a032aeb3a
Hacker Gaucho <84669018+hackergaucho@users.noreply.github.com>
2022-06-22 17:45:33 -0300

    renomear o controller Home

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 require 'vendor/autoload.php';
 $cfg=require 'cfg.php';
 
-use app\controller\Home;
+use app\controller\HomeController;
 use app\lib\Utils;
 
 $Utils=new Utils();
 $Utils->showErrors($cfg['showErrors']);
 
-$Home=new Home();
+$Home=new HomeController();
 $Home->index();
