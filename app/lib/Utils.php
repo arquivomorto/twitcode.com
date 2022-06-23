50164303184d92071a56efdef2d6fd7ef2bdf96d
Hacker Gaucho <84669018+hackergaucho@users.noreply.github.com>
2022-06-23 16:15:17 -0300

    app/lib/Utils.php <- random

--- app/lib/Utils.php
+++ app/lib/Utils.php
@@ -72,6 +72,15 @@ class Utils
             }
         }
     }
+    public function random($tamanho=11)
+    {
+        $str = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-';
+        $randomStr = '';
+        for ($i = 0; $i < $tamanho; $i++) {
+            $randomStr .= $str[rand(0, mb_strlen($str)-1)];
+        }
+        return $randomStr;
+    }
     public function redirect($url)
     {
         header('Location: '.$url);
