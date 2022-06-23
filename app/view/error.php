52c8c17f0e2f48597d7292fa6fcf2a79a098f5ec
Hacker Gaucho <84669018+hackergaucho@users.noreply.github.com>
2022-06-23 20:55:12 -0300

    app/view/error.php

---
+++ app/view/error.php
@@ -0,0 +1,43 @@
+<?php
+$data['title']=__('Erro', false);
+$view('inc/header', $data);
+?>
+<div class="container">
+    <div class="row">
+        <div class="col12">
+            <br><br><br>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col4"></div>
+        <div class="col4">
+            <h1><?php __('Erro');?>
+            </h1>
+            <?php
+                print '<ul>';
+                foreach ($error as $value) {
+                    switch ($value) {
+                        case 'invalidName':
+                            print '<li>'.__('Nome inválido', false).'</li>';
+                        break;
+                        case 'invalidEmail':
+                            print '<li>'.__('Email inválido', false).'</li>';
+                        break;
+                        case 'invalidPassword':
+                            print '<li>'.__('Senha inválida', false).'</li>';
+                        break;
+                        default:
+                            print '<li>'.__('Erro desconhecido', false).'</li>';
+                        break;
+                    }
+                }
+                print '</ul>';
+            ?>
+            <p>
+                <a href="javascript:history.back(-1);">
+                    <?php __('Voltar');?>
+                </a>
+            </p>
+        </div>
+    </div>
+</div>
